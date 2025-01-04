cd65ba055e58051800784e45cd6a4a18a246850f
Xumanstha <shresthaangura@gmail.com>
2025-01-05 00:14:47 +0545

    some new

--- linkedlist.c
+++ linkedlist.c
@@ -1,34 +1,125 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
+
+// Define a structure for a node in the singly linked list
 typedef struct Node
 {
-    struct Node *next;
-    int data;
-}Node;
+    int data;          // Data stored in the node
+    struct Node *next; // Pointer to the next node
+} Node;
+
+// Initialize the head of the list as NULL (empty list)
 Node *head = NULL;
-void insert(int data)
+
+// Function to insert a node at the beginning of the list
+void InsertAtBeginning(int value)
 {
-    Node *newNode = (Node *)malloc(sizeof(Node));
-    newNode->data = data;
-    newNode->next = head;
-    head = newNode;
-}
-void display()
-{
-    Node *temp = head;
-    while (temp != NULL)
+    Node *node = malloc(sizeof(Node)); // Allocate memory for the new node
+    node->data = value;                // Set the data of the new node
+    if (head == NULL)                  // Check if the list is empty
     {
-        printf("%d ", temp->data);
-        temp = temp->next;
+        node->next = NULL; // If empty, the new node points to NULL
+        head = node;       // Make the new node the head
+    }
+    else
+    {
+        node->next = head; // Point the new node to the current head
+        head = node;       // Update the head to the new node
     }
 }
+
+// Function to insert a node at the end of the list
+void InsertAtLast(int value)
+{
+    Node *node = malloc(sizeof(Node)); // Allocate memory for the new node
+    node->data = value;                // Set the data of the new node
+    node->next = NULL;                 // New node's next pointer is NULL
+    if (head == NULL)                  // Check if the list is empty
+    {
+        head = node; // If empty, the new node becomes the head
+    }
+    else
+    {
+        Node *temp = head;         // Temporary pointer to traverse the list
+        while (temp->next != NULL) // Traverse until the last node
+        {
+            temp = temp->next;
+        }
+        temp->next = node; // Set the last node's next to the new node
+    }
+}
+
+// Function to insert a node at a specific position
+void InsertAtX(int value, int loc)
+{
+    Node *node = malloc(sizeof(Node)); // Allocate memory for the new node
+    node->data = value;                // Set the data of the new node
+    int k;
+    if (loc == 0) // If inserting at the beginning
+    {
+        node->next = head; // Point the new node to the current head
+        head = node;       // Update the head to the new node
+        return;
+    }
+    Node *temp = head;        // Temporary pointer to traverse the list
+    for (k = 1; k < loc; k++) // Traverse to the (loc - 1)th node
+    {
+        if (temp == NULL) // If location is invalid (out of bounds)
+        {
+            printf("Invalid Location\n");
+            free(node); // Free the allocated memory
+            return;
+        }
+        temp = temp->next;
+    }
+
+    if (temp == NULL) // Check if location is invalid
+    {
+        printf("Invalid location\n");
+        free(node); // Free the allocated memory
+        return;
+    }
+    node->next = temp->next; // Point the new node to the next node
+    temp->next = node;       // Update the (loc - 1)th node to point to the new node
+}
+
+// Function to traverse and print the list
+void traverse(Node *start)
+{
+    Node *temp = start;  // Start traversing from the given node
+    while (temp != NULL) // Continue until the end of the list
+    {
+        printf("%d\t", temp->data); // Print the data of the current node
+        temp = temp->next;          // Move to the next node
+    }
+    printf("\n"); // Print a newline after traversing
+}
+
+// Function to delete a node from the beginning of the list
+void DeleteAtBeginning()
+{
+    Node *temp;
+    if (head == NULL) // Check if the list is empty
+    {
+        printf("The list is empty\n");
+    }
+    else
+    {
+        temp = head;       // Temporary pointer to the current head
+        head = head->next; // Update the head to the next node
+        free(temp);        // Free the memory of the deleted node
+    }
+}
+
+// Main function to demonstrate the singly linked list operations
 int main()
 {
-    insert(3);
-    insert(4);
-    insert(5);
-    insert(6);
-    insert(7);
-    display();
+    InsertAtBeginning(1); // Insert 1 at the beginning
+    InsertAtBeginning(2); // Insert 2 at the beginning
+    InsertAtBeginning(3); // Insert 3 at the beginning
+    InsertAtLast(10);     // Insert 10 at the end
+    InsertAtX(39, 2);     // Insert 39 at position 2
+    DeleteAtBeginning();  // Delete the first node
+    traverse(head);       // Traverse and print the list
     return 0;
 }
--- stack.c
+++ stack.c
@@ -1,62 +1,94 @@
 #include <stdio.h>
-#define Max 10
-int peek=-1;
+#define Max 5
+int peek = -1;
 int Stack[Max];
 void push(int value)
 {
-    if(peek==Max-1)
+    if (isFull())
     {
         printf("Stack full\n");
     }
     else
     {
-Stack[peek+1]=value;
-peek++;
+        Stack[peek + 1] = value;
+        peek++;
+        printf("%d is pushed into stack\n", value);
     }
 }
 int pop()
 {
-    if(peek==-1)
+    if (isEmpty())
     {
         printf("Stack Empty");
         return 0;
     }
-    else{
-        int value=Stack[peek--];
+    else
+    {
+        int value = Stack[peek--];
         return value;
     }
 }
 int isEmpty()
 {
-    if (peek==-1)
+    return peek == -1;
+}
+int Peek()
+{
+    if (isEmpty())
     {
-        return 1;
-    }
-    else{
-        return 0;
     }
 }
 int isFull()
 {
-    if(peek==Max-1)
-    {
-        return 1;
-    }
-    else{
-        return 0;
-    }
+
+    return peek == Max - 1;
 }
 int main()
 {
-    push(5);
-    push(2);
-    push(3);
-    push(2);
-    push(10);
-    for(int i=0;peek!=-1;i++)
+    // push(5);
+    // push(2);
+    // push(3);
+    // push(2);
+    // push(10);
+    // for(int i=0;peek!=-1;i++)
+    // {
+    //     printf("%d\n",pop());
+    // }
+    int choice, value;
+    do
     {
-        printf("%d\n",pop());
-    }
-  
+        printf("1.Push\n2.Pop\n3.Peek\n4.Exit\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+        {
+            printf("Enter the number to push:\n");
+            scanf("%d", &value);
+            push(value);
+            break;
+        }
+        case 2:
+        {
+            printf("Popped:%d\n", pop());
+            break;
+        }
+        case 3:
+        {
+            printf("The peek value is:%d\n", Peek());
+            break;
+        }
+        case 4:
+        {
+            break;
+        }
+        default:
+        {
+            printf("Invalid choice\n");
+            break;
+        }
+        }
+    } while (choice != 4);
+
     return 0;
 }
